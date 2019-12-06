cb1767f0666d6fb3617b2b2043ec9f0e93f9ff52
Ygor <ygor.maniz@gmail.com>
2019-12-06 02:23:41 -0300

    Implementing navigation actions

--- app/src/main/java/com/github/ymaniz09/animals/view/DetailsAnimalFragment.kt
+++ app/src/main/java/com/github/ymaniz09/animals/view/DetailsAnimalFragment.kt
@@ -6,10 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.github.ymaniz09.animals.R
+import kotlinx.android.synthetic.main.fragment_details_animal.*
 
 class DetailsAnimalFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        listFloatingActionButton.setOnClickListener {
+            val action = DetailsAnimalFragmentDirections.actionGoToList()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/github/ymaniz09/animals/view/ListAnimalsFragment.kt
+++ app/src/main/java/com/github/ymaniz09/animals/view/ListAnimalsFragment.kt
@@ -6,10 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.github.ymaniz09.animals.R
+import kotlinx.android.synthetic.main.fragment_list_animals.*
 
 class ListAnimalsFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        detailsFloatingActionButton.setOnClickListener {
+            val action = ListAnimalsFragmentDirections.actionGoToDetails()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
