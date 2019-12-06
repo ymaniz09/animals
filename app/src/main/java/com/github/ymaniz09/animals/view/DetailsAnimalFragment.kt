01e7ef09bfdc2aba1715a4e7586b91a40cea3457
Ygor <ygor.maniz@gmail.com>
2019-12-06 02:52:17 -0300

    Code cleanup
    Removing FABs added to test navigation actions

--- app/src/main/java/com/github/ymaniz09/animals/view/DetailsAnimalFragment.kt
+++ app/src/main/java/com/github/ymaniz09/animals/view/DetailsAnimalFragment.kt
@@ -2,24 +2,13 @@ package com.github.ymaniz09.animals.view
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-
+import androidx.fragment.app.Fragment
 import com.github.ymaniz09.animals.R
-import kotlinx.android.synthetic.main.fragment_details_animal.*
 
 class DetailsAnimalFragment : Fragment() {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        listFloatingActionButton.setOnClickListener {
-            val action = DetailsAnimalFragmentDirections.actionGoToList()
-            Navigation.findNavController(it).navigate(action)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/github/ymaniz09/animals/view/ListAnimalsFragment.kt
+++ app/src/main/java/com/github/ymaniz09/animals/view/ListAnimalsFragment.kt
@@ -2,24 +2,13 @@ package com.github.ymaniz09.animals.view
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-
+import androidx.fragment.app.Fragment
 import com.github.ymaniz09.animals.R
-import kotlinx.android.synthetic.main.fragment_list_animals.*
 
 class ListAnimalsFragment : Fragment() {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        detailsFloatingActionButton.setOnClickListener {
-            val action = ListAnimalsFragmentDirections.actionGoToDetails()
-            Navigation.findNavController(it).navigate(action)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
