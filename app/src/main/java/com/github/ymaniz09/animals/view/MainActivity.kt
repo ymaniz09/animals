853df2200efdd6037fd5077d7232d45b166d18e5
Ygor <ygor.maniz@gmail.com>
2019-12-06 01:50:10 -0300

    Navigation configuration

--- app/src/main/java/com/github/ymaniz09/animals/view/MainActivity.kt
+++ app/src/main/java/com/github/ymaniz09/animals/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.ymaniz09.animals
+package com.github.ymaniz09.animals.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.ymaniz09.animals.R
 
 class MainActivity : AppCompatActivity() {
 
